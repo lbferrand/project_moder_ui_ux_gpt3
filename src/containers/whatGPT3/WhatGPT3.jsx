2306dde4485ee1da2ae5ea835f410e32b2c46f7f
lbferrand <lbferrand@gmail.com>
2022-06-08 23:31:06 -0400

    Add style to whatgpt3

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -8,8 +8,8 @@ const WhatGPT3 = () => {
       <div className="gpt3__whatgpt3-feature">
         <Feature />
       </div>
-      <div className="gpt3__whatgpt3-header">
-        <h1 className="gradient-text">
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className="gradient__text">
           The possibilities are beyond your imagination
         </h1>
         <p>Explore The Library</p>
