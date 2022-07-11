37e6894a52612cd8d497a626273cd5768c31d5fd
lbferrand <lbferrand@gmail.com>
2022-07-11 12:19:00 -0400

    Update Possibility.jsx

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -11,14 +11,29 @@ const Possibility = () => {
       <div className="gpt3__possibility-content">
         <h4>Request Early Access to Get Started</h4>
         <h1 className="gradient__text">
-          The possibilities are beyond your imagination
+          Build a GPT-3 Discord Chatbot with Node.js
         </h1>
         <p>
-          Yet bed any for travelling assistance indulgence unpleasing. Not
-          thoughts all exercise blessing. Indulgence way everything joy
-          alteration boisterous the attachment. Party we years to order allow
-          asked of.
+          OpenAI’s GPT-3 is a language model that offers a multifaceted “text
+          in, text out” interface that can be applied to virtually any task. It
+          can produce code, stories, recipes, and more. The engine is capable of
+          doing anything - it can even do your homework for you! OpenAI's GPT-3
+          engine is also a remarkable tool for chatbots since its output text is
+          indistinguishable from human text. A fitting integration with this is
+          Discord chatbots.{" "}
         </p>
+        <p>
+          In recent years, Discord has become the central messaging platform for
+          communities especially with the help of Discord bots. Through Discords
+          extensive API, developers are able to build robust bots that are
+          capable of various actions such as automating tasks and chatting with
+          users.
+        </p>
+        <p>
+          With the gaining traction of both of these technologies, I decided to
+          combine them to build an intelligent but fun chatbot.
+        </p>
+
         <h4>Request Early Access to Get Started</h4>
       </div>
     </div>
