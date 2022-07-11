5b491a1452ae99e660e3eaa7e69217a03bec5084
lbferrand <lbferrand@gmail.com>
2022-07-11 10:12:34 -0400

    Update Header.jsx

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -11,10 +11,10 @@ const Header = () => {
           Let’s Build Something amazing with GPT-3 OpenAI
         </h1>
         <p>
-          Yet bed any for travelling assistance indulgence unpleasing. Not
-          thoughts all exercise blessing. Indulgence way everything joy
-          alteration boisterous the attachment. Party we years to order allow
-          asked of.
+          In short, GPT-3 is a language model: a statistical model that
+          calculates the probability distribution over a sequence of words. In
+          other words, GPT-3 is a system for guessing which text comes next when
+          text is given as an input.
         </p>
         <div className="gpt3__header-content__input">
           <input type="email" placeholder="Your Email Address"></input>
